2f7897a0339ff92219447dd9c0eb3bec852f394a
zubairmalek <zubairmalek95@gmail.com>
2022-07-05 17:11:54 +0100

    completed all pages

--- app.js
+++ app.js
@@ -24,4 +24,10 @@ app.get("/works", (request, response) => {
     response.sendFile(__dirname + '/views/works-page.html');
 });
 
+app.get("/photos", (request, response) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+
+
 app.listen(3000, () => console.log("express app listening in port 3000"));
