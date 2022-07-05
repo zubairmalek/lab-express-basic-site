b986ac14c668e8b0c8c576feaff5f53f7f1b7972
zubairmalek <zubairmalek95@gmail.com>
2022-07-05 15:54:57 +0100

    completed first 3 pages

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// We create our own server named app
+// Express server will be handling requests and responses
+const app = express();
+
+//
+// CONFIG
+//
+app.use(express.static('public'));
+
+//Home page
+
+app.get("/", (request, response) => {
+    // response.send("this is string");
+    response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + '/views/about-page.html');
+});
+
+app.get("/works", (request, response) => {
+    response.sendFile(__dirname + '/views/works-page.html');
+});
+
+app.listen(3000, () => console.log("express app listening in port 3000"));
